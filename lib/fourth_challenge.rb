2f4af6f6cb33f66c9a95c46d3afcf2b4344b7274
wolfene2001 <wolfene2001@yahoo.com>
2019-11-13 19:20:55 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attitude: "worried"},
         {"Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   
+
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -17,7 +17,7 @@ def fourth_challenge
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
-   
+
  }
 }
 end
